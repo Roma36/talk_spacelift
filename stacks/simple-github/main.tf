af7ffe7ef83522c8996b9a75ca09bbf2401ec671
Roman Grinovski <roman.grinovski@gmail.com>
2024-10-23 16:57:28 +0200

    Update main.tf

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -2,9 +2,7 @@ provider "github" {
   owner = "Roma36"
 }
 
-resource "github_repository" "my_repo" {
-  name        = "tf_sp_example"
-  description = "My new repository for Terraform"
-
-  visibility = "public"
+output "test" {
+  value = "test"
 }
+
